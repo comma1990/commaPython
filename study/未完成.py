c5a457fcae17a3af4c259463c489d8daf715c739
comma1990 <981638035@qq.com>
2020-10-16 19:30:00 +0800

    未完成

---
+++ study/未完成.py
@@ -0,0 +1,9 @@
+#author : comma
+#date : 2020/10/16 19:28
+
+#查询商品信息接口：http://productquery.iapi.ymatou.com/api/Product/GetProductDetailListByTradeIsolation
+    # {
+    #     "ProductIdList":["p6400551"],
+    #     "AppId":"evtadmin.ymatou.cn",
+    #     "NextActivityExpire":17
+    # }

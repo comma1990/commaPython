cc0524be8c0dc842c162deb4124894056bb48726
comma1990 <981638035@qq.com>
2020-10-19 18:40:46 +0800

    优化创建活动是拉取商品列表的接口

--- merchant_pc/getProduceList.py
+++ merchant_pc/getProduceList.py
@@ -1,13 +1,13 @@
 # Author ： comma
 # 日期 : 2020/10/16  23:42
 
-# 获取商品列表：https://www.shop2cn.com/service/mgmt/api/page/produceList?status=1&pageIndex=1&pageSize=20
+# 获取++创建活动时++拉取的商品列表
 import requests
 import urllib3
 import json
 import openpyxl
 
-def getPorductList(pageIndex): # pageIndex页码，你想获得哪页的数据
+def getPorduceList(pageIndex): # pageIndex页码，你想获得哪页的数据
     from merchant_pc.getCookie import getCookies  # cookie获取提取到方法外，调用该方法必须穿cookie
     Cookie = getCookies()
     #text/html,application/xhtml+xml,application/xml
@@ -29,9 +29,24 @@ def getPorductList(pageIndex): # pageIndex页码，你想获得哪页的数据
     return productList
 
 if __name__ == '__main__':
+    wk=openpyxl.Workbook()
+    sheet=wk.create_sheet('活动商品列表')
+    x=0     # 计数器
+    n=int(input('请输入你想添加的商品个数：'))
+    #discount = 5.6  # 设置折扣
+    discount=int(input('请输入折扣：'))
     # 遍历商品列表，并在商品规格上添加活动库存信息
-    for item in getPorductList(2):   #遍历商品列表
-        for catlog in item['catalogList']:  #遍历规格列表
-            catlog['activityRealStock']=0    #添加活动真实库存
-            catlog['activityStock']=0    # 添加活动库存 ——为0的时候默认使用全部库存
-        print(catlog)
+    for item in getPorduceList(2):   #遍历商品列表
+        from merchant_pc.getPoductActivityInfo import getPoductActivityInfo
+        if getPoductActivityInfo(item['id'])==None:     #判断商品没有活动信息后，添加规格的活动信息，并添加到Excel表中
+            for catlog in item['catalogList']:  #遍历规格列表
+                catlog['activityRealStock']=0    #添加活动真实库存
+                catlog['activityStock']=0    # 添加活动库存 ——为0的时候默认使用全部库存
+                catlog['discount']=discount     # 添加折扣信息
+                catlog['discountPrice']=catlog['salePrice']*discount/10   # 添加折后价格
+                x+=1
+                if x>n:
+                    break
+                sheet.append([item['id'],str(catlog).replace('\'','"')])
+    wk.save('D:\python\study\活动商品列表.xlsx')
+        #print(catlog)

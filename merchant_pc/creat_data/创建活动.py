7df2f7513ab55b795eb810380adcaec6dc85e4a2
comma1990 <981638035@qq.com>
2020-10-26 18:07:01 +0800

    优化活动创建的方法-可以成功创建了

--- merchant_pc/creat_data/创建活动.py
+++ merchant_pc/creat_data/创建活动.py
@@ -6,30 +6,13 @@
         ②从商品列表中获取没有活动信息的商品getgetNoActivityProductId-----（调用了获取商品活动信息的方法getPoductActivityInfo）
         ③向每个规格中添加活动信息，作为活动商品数据'''
 '''商品参数信息
-{"productId": "p7084378", "catalogList":
-                       [{"catalogId": "c43699135", "buyingPrice": 0, "salePrice": 0.1, "stockNum": 100, "realStock": 100,
-                         "virtualStock": 0, "propertyValue": "白色", "sku": None, "barcode": None, "counterPrice": 0,
-                         "marketAmount": 0, "activityRealStock": 0, "activityStock": 0, "discount": "5.6",
-                         "discountPrice": "0.06"},
-                        {"catalogId": "c43699134", "buyingPrice": 0, "salePrice": 0.2, "stockNum": 200,
-                         "realStock": 200, "virtualStock": 0, "propertyValue": "黑色", "sku": None, "barcode": None,
-                         "counterPrice": 0, "marketAmount": 0, "activityRealStock": 0, "activityStock": 0,
-                         "discount": "5.6", "discountPrice": "0.11"}]},
-                                   {"productId": "p7084377", "catalogList": [
-                                       {"catalogId": "c43698270", "buyingPrice": 0, "salePrice": 0.1, "stockNum": 100,
-                                        "realStock": 100, "virtualStock": 0, "propertyValue": "白色", "sku": None,
-                                        "barcode": None, "counterPrice": 0, "marketAmount": 0, "activityRealStock": 0,
-                                        "activityStock": 0, "discount": "5.6", "discountPrice": "0.06"},
-                                       {"catalogId": "c43698269", "buyingPrice": 0, "salePrice": 0.2, "stockNum": 200,
-                                        "realStock": 200, "virtualStock": 0, "propertyValue": "黑色", "sku": None,
-                                        "barcode": None, "counterPrice": 0, "marketAmount": 0, "activityRealStock": 0,
-                                        "activityStock": 0, "discount": "5.6", "discountPrice": "0.11"}]}
+{"productId": "p7084378", "catalogList":[{"catalogId": 。。。。}]}
 '''
 import requests
 import urllib3
-import json
-import openpyxl
-import xlrd
+import random
+import datetime
+
 
 def createXsq(productList):
     from merchant_pc.getCookie import getCookies  # cookie获取提取到方法外，调用该方法必须穿cookie
@@ -40,14 +23,16 @@ def createXsq(productList):
                'Connection': 'keep - alive',
                'Cookie': Cookie
                }
-    from merchant_pc.getTime import getAddMinutesTime2,getAddDaysTime2,getAddDaysTime3,getAddMinutesTime3
-    beginTime=getAddMinutesTime3()
-    endTime=getAddDaysTime3()
-    promotionBeginTime=getAddMinutesTime2()
-    promotionTimeEndTime=getAddDaysTime2()
+    from merchant_pc.getTime import getAddMinutesTime2, getAddDaysTime2, getAddDaysTime3, getAddMinutesTime3
+    beginTime = getAddMinutesTime3()
+    endTime = getAddDaysTime3()
+    promotionBeginTime = getAddMinutesTime2()
+    promotionTimeEndTime = getAddDaysTime2()
 
     '''设置请求数据'''
-    requestData = {"name": "py测试", "promotionTime": [promotionBeginTime, promotionTimeEndTime],
+    # x=random.randint(1,100) # 定义一个随机数用于创建活动名称使用
+    date = datetime.datetime.now().strftime('%Y%m%d-%H%M')  # 获取当前时间用作创建活动名称使用
+    requestData = {"name": f"限时抢{date}号", "promotionTime": [promotionBeginTime, promotionTimeEndTime],
                    "limitType": 1, "limitNum": "", "previewTime": 0, "preTime": "",
                    "beginTime": beginTime, "endTime": endTime,
                    "productList": productList,
@@ -57,37 +42,39 @@ def createXsq(productList):
                          headers=headers,
                          verify=False)
     response = data.text
-    #depotId = json.loads(response)['data']['depotList'][1]['depotId']
     print(response)
-    #return response
+    # return response
+
 
 if __name__ == '__main__':
-    # wk=openpyxl.Workbook()
-    # sheet=wk.create_sheet('活动商品列表')
-    productList=[]  #定义一个空列表接收商品id和规格信息
-    dic={}  #定义一个字典接收商品id和规格信息，将字典添加到列表中
-    x=0     # 计数器
-    sellerid=500002398 #商户id，查询商品信息需要
-    n=int(input('请输入你想添加的商品个数：'))
-    #discount = 5.6  # 设置折扣
-    discount=int(input('请输入折扣：'))
+    productList = []  # 定义一个空列表接收商品id和规格信息
+    x = 0  # 计数器
+    sellerid = 500002398  # 商户id，查询商品信息需要
+    n = int(input('请输入你想添加的商品个数：'))
+    discount = eval(input('请输入折扣：'))  # 设置折扣
     # 遍历商品列表，并在商品规格上添加活动库存信息
-    from merchant_pc.getProduceList import getPorduceList   #获取商品列表（创建活动时拉取的列表）
-    for item in getPorduceList(2):   #遍历商品列表
+    from merchant_pc.getProduceList import getPorduceList  # 获取商品列表（创建活动时拉取的列表）
+
+    for item in getPorduceList(2):  # 遍历商品列表
+        productid = [item['id']]
+        dic = {}  # 定义一个字典接收商品id和规格信息，将字典添加到列表中
+        cateloglist = []  # 定义一个空规格列表，接收规格信息
         from merchant_pc.getPoductActivityInfo import getPoductActivityInfo
-        if bool(getPoductActivityInfo(sellerid,item['id']))==False:     #判断商品没有活动信息后，添加规格的活动信息，并添加到Excel表中
-            for catlog in item['catalogList']:  #遍历规格列表
-                catlog['activityRealStock']=0    #添加活动真实库存
-                catlog['activityStock']=0    # 添加活动库存 ——为0的时候默认使用全部库存
-                catlog['discount']=discount     # 添加折扣信息
-                catlog['discountPrice']=catlog['salePrice']*discount/10   # 添加折后价格
-                dic['productId']=item['id']
-                dic['catalogList']=catlog
-                productList.append(dic)
-                x+=1
-                if x>n:
-                    break
+
+        if bool(getPoductActivityInfo(sellerid, productid)) == False:  # 判断商品没有活动信息后，添加规格的活动信息
+            for catlog in item['catalogList']:  # 遍历规格列表
+                # cateloglist = []  # 定义一个空规格列表，接收规格信息
+                catlog['activityRealStock'] = 0  # 添加活动真实库存
+                catlog['activityStock'] = 0  # 添加活动库存 ——为0的时候默认使用全部库存
+                catlog['discount'] = discount  # 添加折扣信息
+                catlog['discountPrice'] = catlog['salePrice'] * discount / 10  # 添加折后价格
+                cateloglist.append(catlog)
+            dic['productId'] = item['id']
+            dic['catalogList'] = cateloglist
+            x += 1
+            if x > n:
+                break
+            productList.append(dic)
+
+        # print(productList)
     createXsq(productList)
-                # sheet.append([item['id'],str(catlog).replace('\'','"')])
-    wk.save('D:\python\study\活动商品列表.xlsx')
-    # 表中的数据都是添加好活动信息的，现在需要提取出来，id和category组合作为请求参数
--- merchant_pc/getPoductActivityInfo.py
+++ merchant_pc/getPoductActivityInfo.py
@@ -9,10 +9,6 @@ import json
 
 
 def getPoductActivityInfo(sellerId,productId):  # productId ["p4740703", "p4740702", "p4706337", "p4740694"]
-    headers = {'Accept': 'application/json, text/plain, */*',
-               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
-               'Connection': 'keep - alive',
-               }
     '''设置请求数据'''
     requestdata = {
         "sellerId": sellerId,
@@ -21,11 +17,10 @@ def getPoductActivityInfo(sellerId,productId):  # productId ["p4740703", "p47407
     urllib3.disable_warnings()
     data = requests.post('http://shopbiz.iapi.ymatou.com/api/page/getDiscountProductList',
                          json=requestdata,
-                         headers=headers,
                          verify=False)
     response = data.text
     acticityInfo = json.loads(response)['data']['productList']  # productlist为空的时候表明商品没有活动
-    print(response)
+    #print(response)
     return acticityInfo
 
 

a77e2fcf680d0230d0b5070f7bcd11182afff253
comma <981638035@qq.com>
2020-10-31 20:39:47 +0800

    整理

--- study/demo.py
+++ study/demo.py
@@ -2,33 +2,6 @@
 # date : 2020/10/15 10:43
 
 
-########列表的操作：#################
-# 排序
-# list1 = [0, 9, 5, 7, 2, 3]
-# list1.remove(2) # 移除指定的元素
-# list1.pop(1)    # 根据索引移除元素，如果不指定索引位置，将列表的最后一个元素移除
-# list1.clear() # 清空列表
-# list2=list1[1:3]    # 切片，取指定索引范围内的元素，删除指定索引范围内的数据：list1[1:3]=[]
-# del list1    # 删除列表对象
-# print(list1)
-
-# list1.sort()    # 升序
-# list1.sort(reverse=True)    # 倒序
-# print(list1)
-# print(sorted(list1))    # 内置函数
-# print(sorted(list1,reverse=True))   # 先升序排序，然后翻转，就是倒序
-
-# 列表表达式
-# list2=[i*i for i in range(1,10,2)]  # 范围1-10，步长为2，遍历结果是1，3，5，7，9；遍历结果自相乘
-# print(list2)
-
-
-####################################
-
-
-# #空列表的bool值为False
-# x=[]
-# print(bool(x))
 
 
 # n=int(input('请输入次数'))
@@ -47,30 +20,39 @@
 #     print()
 
 
-# 打印水仙花数
-# for item in range(1,1000):
-#     ge=item%10
-#     shi=item//10%10
-#     bai=item//100
-#     if ge**3+shi**3+bai**3==item:
-#         print(item,end='\t')
-
+# 打印水仙花数  个位数的3次方+十位数的三次方+百位数的三次方=本身
+for item in range(1,1000):
+    ge=item%10
+    shi=item//10%10
+    bai=item//100
+    if ge**3+shi**3+bai**3==item:
+        print(item,end='\t')
+print('\n----------------------')
 
 # #   打印26个字母
-# for item in range(26):
-#     print(chr(item+ord('a')))
+for item in range(26):
+    print(chr(item + ord('a')), end='\t')
+
+print('\n-------------------')
 
 # 时间处理
-# import time
-# import datetime
-# def getAddMinutesTime():
-#     data=datetime.datetime.now()
-#     offset=datetime.timedelta(minutes=2)
-#     result=(data+offset).strftime('%Y-%m-%dT%H:%M:%S')
-#     return result
-#
-# def getAddMinutesTime():
-#     data=datetime.datetime.now()
-#     offset=datetime.timedelta(days=2)
-#     result=(data+offset).strftime('%Y-%m-%dT%H:%M:%S')
-#     return result
+import time
+import datetime
+
+
+def getAddMinutesTime():
+    data = datetime.datetime.now()
+    offset = datetime.timedelta(minutes=2)
+    result = (data + offset).strftime('%Y-%m-%dT%H:%M:%S')
+    return result
+
+
+def getAddDaysTime():
+    data = datetime.datetime.now()
+    offset = datetime.timedelta(days=2)
+    result = (data + offset).strftime('%Y-%m-%dT%H:%M:%S')
+    return result
+
+
+print(getAddMinutesTime())
+print(getAddDaysTime())
---
+++ study/列表.py
@@ -0,0 +1,30 @@
+# @Author  :  comma 
+# @Date    :  2020-10-31 20:38
+
+
+########列表的操作：#################
+# 排序
+list1 = [0, 9, 5, 7, 2, 3]
+list1.remove(2)  # 移除指定的元素
+list1.pop(1)  # 根据索引移除元素，如果不指定索引位置，将列表的最后一个元素移除
+list1.clear()  # 清空列表
+list2 = list1[1:3]  # 切片，取指定索引范围内的元素，删除指定索引范围内的数据：list1[1:3]=[]
+del list1  # 删除列表对象
+print(list1)
+
+list1.sort()  # 升序
+list1.sort(reverse=True)  # 倒序
+print(list1)
+print(sorted(list1))  # 内置函数
+print(sorted(list1, reverse=True))  # 先升序排序，然后翻转，就是倒序
+
+# 列表表达式
+list2 = [i * i for i in range(1, 10, 2)]  # 范围1-10，步长为2，遍历结果是1，3，5，7，9；遍历结果自相乘
+print(list2)
+
+####################################
+
+
+# #空列表的bool值为False
+x = []
+print(bool(x))

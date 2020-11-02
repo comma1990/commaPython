db25f8f1a33f400c259476e9fec8d96622b77007
comma <981638035@qq.com>
2020-11-03 00:13:47 +0800

    11.2最后一次提交

--- study/类与对象.py
+++ study/类与对象.py
@@ -23,11 +23,11 @@ class Student:  # 类名以大写字母开头
 
 # 创建类的实例化对象
 stu1 = Student('土豆', 13)  # 因为类的初始化方法定义了两个参数，所以创建类对象的时候必须传两个参数
-stu1.gender='男' # 为对象stu1创建变量，只在stu1这个对象中可用，Student创建的其它对象不可以用
-print(stu1.name,stu1.gender)
+stu1.gender = '男'  # 为对象stu1创建变量，只在stu1这个对象中可用，Student创建的其它对象不可以用
+print(stu1.name, stu1.gender)
 stu1.function('憨憨')  # 对象名.方法名(参数)
 stu1.staticFun()
-stu1.classmd()
+stu1.classmd('测试参数')
 print('-----------下面是方法的另一种调用方式--------------')
 Student.function(stu1, '渣渣')  # 类方法调用的另种方式，类名.方法(实例化对象，参数)  --->实际上实例化对象就是方法定义出的self
 print('-----------------------')
@@ -41,3 +41,12 @@ Student.classmd('args参数')  # 直接通过 类名.类方法名 使用，不�
 
 print('-----------静态方法的使用------------')
 Student.staticFun()  # 直接通过 类名.方法名 调用，不需要实例化方法
+
+
+# 定在类之外的函数，并赋值给对象方法
+def show():
+    print("定义在类之外的，称为函数")
+
+
+stu1.show = show    # 次数函数后面不能加括号（）
+stu1.show()

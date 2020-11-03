2d82a3b3738214b867333f52decc8d26ace00466
comma1990 <981638035@qq.com>
2020-11-03 19:07:05 +0800

    11月03下班提交

--- study/面向对象的三大特征.py
+++ study/面向对象的三大特征.py
@@ -10,18 +10,47 @@
 
 '''
 
-class Comma:
-    def __init__(self,name,age):
-        self.name=name
-        self.__age=age  # 私有化属性，外部不可以使用
+
+class Comma(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age  # 私有化属性，外部不可以使用
+
     def show(self):
-        print(self.name,self.__age)
+        print(self.name, self.__age)
 
 
-com=Comma('逗号',30)
+com = Comma('逗号', 30)
 com.show()
-print(com.name) #在类的外部使用类属性
+print(com.name)  # 在类的外部使用类属性
 # print(com.__age)  # 私有化的属性，外部不能使用
 
 # print(dir(com))
 print(com._Comma__age)  # 在类的外部可以通过 _类名__类属性 的方式进行访问私有变量
+print('---------------------------')
+
+
+#################### 继承 ################
+class Stone(Comma):
+    def __init__(self, name, age, gender):
+        super(Stone, self).__init__(name, age)
+        self.gender = gender
+
+    def show(self):  # 重写show方法
+        super(Stone, self).show()  # 继承父类的show方法
+        print(self.gender)  # 增加自己特有的处理
+
+
+stone = Stone('石头', 28, '男')  # 创建子类Stone的实例方法
+stone.show()  # 子类继承了父类的方法show()
+
+# 多继承
+class sun(Comma,Stone): # sun同时继承了Comma和Stone，继承这两个类的所有对象方法
+    pass
+
+
+############## 方法重写 ###############
+"""
+如果子类对继承自父类的某个属性或者方法不满意，可以在子类中对其（方法体）进行重新编写
+子类重写丰厚的方法中可以通过super().xxx()调用父类中被重新的方法
+"""
